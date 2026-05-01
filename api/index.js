0a0c7bd48263c2de57d5fb3bef9d16cc68a1283e
omveersingh409 <omveersinghshekhawat20@gmail.com>
2026-05-02 03:10:19 +0530

    Add debug handler to api/index.js

--- api/index.js
+++ api/index.js
@@ -1,2 +1,7 @@
-const app = require('../backend/server.js');
+let app;
+try {
+  app = require('../backend/server.js');
+} catch (error) {
+  app = (req, res) => res.status(500).json({ error: error.message, stack: error.stack });
+}
 module.exports = app;
